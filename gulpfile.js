ddd92b596e3a376c7cf85114945dd1c9e510d979
Ryo Aita <ryo.aita@gunosy.com>
2015-04-20 14:50:18 +0900

    Remove the loading animation and some cosmetic change

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 gulp.task('scripts', function() {
-  return gulp.src(['./src/*.js'])
+  return gulp.src(['./src/jsmpg.js', './src/*.js'])
     .pipe(concat('jsmpg.js'))
     .pipe(gulp.dest('./'));
 });
---
+++ src/utils.js
@@ -0,0 +1,52 @@
+// ----------------------------------------------------------------------------
+// Utilities
+
+jsmpeg.prototype.readCode = function(codeTable) {
+  var state = 0;
+  do {
+	state = codeTable[state + this.buffer.getBits(1)];
+  } while( state >= 0 && codeTable[state] != 0 );
+  return codeTable[state+2];
+};
+
+jsmpeg.prototype.findStartCode = function( code ) {
+  var current = 0;
+  while( true ) {
+	current = this.buffer.findNextMPEGStartCode();
+	if( current == code || current == BitReader.NOT_FOUND ) {
+	  return current;
+	}
+  }
+  return BitReader.NOT_FOUND;
+};
+
+jsmpeg.prototype.fillArray = function(a, value) {
+  for( var i = 0, length = a.length; i < length; i++ ) {
+	a[i] = value;
+  }
+};
+
+jsmpeg.prototype.cachedFrameCount = 0;
+jsmpeg.prototype.calculateFrameCount = function() {
+  if( !this.buffer || this.cachedFrameCount ) {
+	return this.cachedFrameCount;
+  }
+
+  // Remember the buffer position, so we can rewind to the beginning and
+  // reset to the current position afterwards
+  var currentPlaybackIndex = this.buffer.index,
+	  frames = 0;
+
+  this.buffer.index = 0;
+  while( this.findStartCode(START_PICTURE) !== BitReader.NOT_FOUND ) {
+	frames++;
+  }
+  this.buffer.index = currentPlaybackIndex;
+
+  this.cachedFrameCount = frames;
+  return frames;
+};
+
+jsmpeg.prototype.calculateDuration = function() {
+  return this.calculateFrameCount() * (1/this.pictureRate);
+};
