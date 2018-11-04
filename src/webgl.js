3cadc27b0cc0b640c9c6ba3d83e9af88caf4caf2
phoboslab <dominic@phoboslab.org>
2018-11-04 21:24:11 +0100

    Fix stretched WebGL viewport when video with is not a multiple of 16; close #254

--- src/webgl.js
+++ src/webgl.js
@@ -83,7 +83,9 @@ WebGLRenderer.prototype.resize = function(width, height) {
 	this.canvas.height = this.height;
 
 	this.gl.useProgram(this.program);
-	this.gl.viewport(0, 0, this.width, this.height);
+
+	var codedWidth = ((this.width + 15) >> 4) << 4;
+	this.gl.viewport(0, 0, codedWidth, this.height);
 };
 
 WebGLRenderer.prototype.createTexture = function(index, name) {
